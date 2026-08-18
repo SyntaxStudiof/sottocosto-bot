cfff4bcfcc65036cc8656371a5c402fabe9e1106
SyntaxStudiof <syntaxstudio.f@gmail.com>
2026-08-18 17:39:37 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,22 +9,21 @@ def clean_title(raw_text):
     
     # 2. Rimuovi TUTTE le etichette di prezzo
     text = re.sub(r'[Pp]rezzo\s*[a-zA-Z\s]*?[:.]?\s*\d+[.,]?\d*\s?[€€]\s*', '', text)
+    text = re.sub(r'\b\d+[.,]?\d*\s?[€€]\b', '', text)
     
-    # 3. Rimuovi TUTTI gli URL (link Amazon, ecc.)
+    # 3. Rimuovi tutti gli URL (link Amazon, ecc.)
     text = re.sub(r'https?://\S+', '', text)
     
-    # 4. Rimuovi TUTTE le parentesi e TUTTO il loro contenuto (es: "[VAI ALL']", "(Disclamer)")
-    text = re.sub(r'\[.*?\]', '', text)  # Rimuove le parentesi quadre e ciò che c'è dentro
-    text = re.sub(r'\(.*?\)', '', text)  # Rimuove le parentesi tonde e ciò che c'è dentro
-    
-    # 5. Rimuovi eventuali parole chiave rimaste come "VAI ALL'OFFERTA" o simili
+    # 4. Rimuovi frasi comuni lasciate dai canali
     text = re.sub(r'VAI ALL\'OFFERTA', '', text, flags=re.IGNORECASE)
     text = re.sub(r'VAI ALL\'', '', text, flags=re.IGNORECASE)
-    
-    # 6. Rimuovi hashtag o tag rimasti
     text = re.sub(r'#\w+', '', text)
     
-    # 7. Pulisci gli spazi extra
+    # 5. IL TRUCCO FINALE: Cancella TUTTI i simboli di parentesi tonde, quadre e graffe
+    # Questa riga elimina la ( anche se è rimasta da sola, senza cercare la sua compagna )
+    text = re.sub(r'[\(\)\[\]\{\}]', '', text)
+    
+    # 6. Pulisci gli spazi extra
     text = ' '.join(text.split())
     
     return text.strip()
